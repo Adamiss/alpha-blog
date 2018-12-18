7670803475bdaae863517940aac54feedf04af8b
Adam <Adamiss@users.noreply.github.com>
2018-12-18 11:34:12 +0100

    Fix create and list categories tests

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,23 +1,17 @@
 require 'test_helper'
-require 'rails-controller-testing'
-
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
-
-  def setup
-    @category = Category.create(name: "sports")
-  end
-
   test "get new category form and create category" do
     get new_category_path
     assert_template 'categories/new'
-    assert_difference'Category.count',1 do
-      post categories_path, params: { category: { name: "sports" } }
+    assert_difference 'Category.count', 1 do
+      post categories_path, params: { category: {name: "sports"}}
       follow_redirect!
-
     end
     assert_template 'categories/index'
-    assert_match "sports", response.body
+    assert_match 'sports', response.body
   end
+
+
 
   test "invalid category submission results in failure" do
     get new_category_path
@@ -32,4 +26,3 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
   end
 
 end
-
