b7964bb1eb396d271fe16d12b2f45d48871ed94d
Adam <Adamiss@users.noreply.github.com>
2018-12-18 11:22:34 +0100

    Add categories model tests

--- test/integration/list_categories_test.rb
+++ test/integration/list_categories_test.rb
@@ -1,35 +1,20 @@
 require 'test_helper'
-require 'rails-controller-testing'
+
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
 
   def setup
-    @category = Category.create(name: "sports")
+    @category = Category.create(name: "books")
+    @category2 = Category.create(name: "programming")
   end
 
-  test "get new category form and create category" do
-    get new_category_path
-    assert_template 'categories/new'
-    assert_difference'Category.count',1 do
-      post categories_path, params: { category: { name: "sports" } }
-      follow_redirect!
-
-    end
+  test "should show categories listing" do
+    get categories_path
     assert_template 'categories/index'
-    assert_match "sports", response.body
+    assert_select "a[href=?]", category_path(@category), text: @category.name
+    assert_select "a[href=?]", category_path(@category2), text:@category2.name
   end
 
-  test "invalid category submission results in failure" do
-    get new_category_path
-    assert_template 'categories/new'
-    assert_no_difference'Category.count' do
-      post categories_path, params: {category:{name:" "}}
-
-    end
-    assert_template 'categories/new'
-    assert_select 'h2.panel-title'
-    assert_select 'div.panel-body'
-  end
 
 end
 
